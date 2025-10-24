37663feec99001221f926cf5c560e40a9f09c5c7
Cody Cagampang <codycagampang@gmail.com>
2025-10-24 13:53:24 +0100

    Added the routes for the login and registration

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,7 +5,7 @@ const ctrlOthers = require('../controllers/others');
 const ctrlTeams = require('../controllers/teamsList');
 const ctrlWestStats = require('../controllers/westTeamStats');
 const ctrlEastStats = require('../controllers/eastTeamStats');
-
+const ctrlAuthentication = require('../controllers/authentication');
 /* Display teams on front page */
 router.get('/', ctrlTeams.teamsList);
 
@@ -14,6 +14,13 @@ router.get('/stats/west', ctrlWestStats.westTeamStats);
 
 /* Display Win/Loss record of Eastern Conference Teams */
 router.get('/stats/east', ctrlEastStats.eastTeamStats);
+
+/* Display Register Page */
+router.get('/register', ctrlAuthentication.register);
+
+/* Display Login Page */
+router.get('/login', ctrlAuthentication.login);
+
 /* Other pages */
 router.get('/about', ctrlOthers.about);
 
