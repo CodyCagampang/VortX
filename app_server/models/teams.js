af9feca5084c395d5a3785aaa78eae7afcc84497
Cody Cagampang <codycagampang@gmail.com>
2025-11-12 14:22:17 +0000

    Created a teams.js file in the models folder. Included validation to ensure reliability and prevent redundant code whilst also including team information

---
+++ app_server/models/teams.js
@@ -0,0 +1,52 @@
+const { Double } = require('mongodb');
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    number: {
+        type: Number,
+        min: 0,
+        max: 99
+    },
+
+    position: {
+        type: String,
+        enum: ['PG', 'SG', 'SF', 'PF', 'C'], // https://masteringjs.io/tutorials/mongoose/enum
+        required: true
+    },
+
+    height: String,
+    age: {
+        type: Number,
+        min: 18,
+        max: 42
+    } 
+});
+
+const teamSchema = new mongoose.Schema({ 
+    name: { 
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    conference: {
+        type: String,
+        enum: ["East", "West", "Eastern", "Western"]
+    },
+
+    coach: String,
+    
+    wins, losses: {
+        type: number,
+        min: 0,
+        max: 82
+    },
+
+    players: [playerSchema]
+
+});
