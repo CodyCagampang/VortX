a532aa1b80f924a783079a23ff72e98de4044499
Cody Cagampang <codycagampang@gmail.com>
2025-11-12 14:20:55 +0000

    Created a DB connection string for only the teams.js folder for now. Successfully connected to the mongoose database

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose');
+
+const dbURI = "mongodb+srv://codycagampang_db_user:o(>!ZT8D4C69@cluster0.xyrzwtm.mongodb.net/?appName=Cluster0";
+try {
+   
+mongoose.connect(
+    dbURI,
+    { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => {console.log(" Mongoose is connected")},
+	err=> {console.log(err)}
+	);
+}
+ catch (e) {
+  console.log("could not connect");
+}
+
+
+
+require('./teams');
